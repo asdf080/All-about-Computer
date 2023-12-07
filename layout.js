44329974f06823f456156c0efa51fd820e7c7508
Dahee Lee <ai04120@naver.com>
2023-12-07 22:47:48 +0900

    6

--- layout.js
+++ layout.js
@@ -14,3 +14,35 @@ function add(){
 }
 
 let intv = setInterval(add, 200);
+
+function searching(event) {
+  if (event.key === "Enter") {
+      window.open("https://www.google.com/search?q=" + encodeURIComponent(document.querySelector("#search").value),"_blank");
+  }
+}
+
+
+function nightDayHandler(self){
+  var target = document.querySelector('body');
+  if(self.value === 'night'){
+    target.style.backgroundColor = 'black';
+    target.style.color = 'white';
+    self.value = 'day';
+    var alist = document.querySelectorAll('a');
+    var i = 0;
+    while(i < alist.length){
+      alist[i].style.color = 'powderblue';
+      i = i + 1;
+    }
+  } else {
+    target.style.backgroundColor = 'white';
+    target.style.color = 'black';
+    self.value = 'night';
+    var alist = document.querySelectorAll('a');
+    var i = 0;
+    while(i < alist.length){
+      alist[i].style.color = 'blue';
+      i = i + 1;
+    }
+  }
+}
