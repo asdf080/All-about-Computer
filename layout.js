e365756df194627e6992c3fb33dd181221e7f194
Dahee Lee <ai04120@naver.com>
2023-12-08 14:29:51 +0900

    addCursor

--- layout.js
+++ layout.js
@@ -1,3 +1,4 @@
+// 제목 타이핑
 let txt = "ALL ABOUT COMPUTER";
 let i = 0;
 
@@ -15,12 +16,14 @@ function add(){
 
 let intv = setInterval(add, 200);
 
+// 검색창
 function searching(event) {
   if (event.key === "Enter") {
       window.open("https://www.google.com/search?q=" + encodeURIComponent(document.querySelector("#search").value),"_blank");
   }
 }
 
+// 야간모드
 let clickNum = 0;
 function nightDayHandler(){
   clickNum++;
@@ -34,9 +37,11 @@ function nightDayHandler(){
     }
   } else if(clickNum % 2 === 0) {
     document.querySelector('body').style.backgroundImage = "url('./images/zxcv.png')";
-    
     for (let a of document.querySelectorAll('#main_lnb a, .main_article p, .description *')){
       a.style.color = ''
+    }
+    for (let a of document.querySelectorAll('.main_article i')){
+      a.style.color = '#6c5ce7'
     }
     for (let a of document.querySelectorAll('.main_article h1, #side_title')){
       a.style.color = ''
@@ -44,15 +49,6 @@ function nightDayHandler(){
     for(let a of document.querySelectorAll('.main_article, .item, #goto-top')){
       a.style.backgroundColor = ''
     }
-
-    for (let a of document.querySelectorAll('#main_lnb a')){
-      a.addEventListener('mouseenter', () => {
-      a.classList.add('hover-effect');
-    });
-      a.addEventListener('mouseleave', () => {
-      a.classList.remove('hover-effect');
-    });
-   }
   }
 }
 
